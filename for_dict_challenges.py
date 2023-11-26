b5f643dd93e566b44b67eea8198f9678cade2be0
satrn8 <verypyc@gmail.com>
2023-11-27 00:00:57 +0300

    add file "dict_challenges"

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -4,29 +4,48 @@
 # Вася: 1
 # Маша: 2
 # Петя: 2
+def count_student():
+    students = [
+        {'first_name': 'Вася'},
+        {'first_name': 'Петя'},
+        {'first_name': 'Маша'},
+        {'first_name': 'Маша'},
+        {'first_name': 'Петя'}
+    ]
 
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Петя'},
-]
-# ???
+    counts = {}
+    for student in students:
+        if student["first_name"] not in counts.keys():
+            counts[student["first_name"]] = 1
+        else:
+            counts[student["first_name"]] += 1
+
+    for student, count in counts.items():
+        print(f"{student}: {count}")
 
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Оля'},
-]
-# ???
+def popular_name():
+    students = [
+        {'first_name': 'Вася'},
+        {'first_name': 'Петя'},
+        {'first_name': 'Маша'},
+        {'first_name': 'Маша'},
+        {'first_name': 'Оля'},
+    ]
+
+    counts = {}
+    for student in students:
+        if student["first_name"] not in counts.keys():
+            counts[student["first_name"]] = 1
+        else:
+            counts[student["first_name"]] += 1
+
+    max_count = max(counts, key=counts.get)
+    print(f"Самое частое имя среди учеников: {max_count}, оно встречается {counts.get(max_count)}")
 
 
 # Задание 3
@@ -35,23 +54,36 @@ students = [
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
-school_students = [
-    [  # это – первый класс
-        {'first_name': 'Вася'},
-        {'first_name': 'Вася'},
-    ],
-    [  # это – второй класс
-        {'first_name': 'Маша'},
-        {'first_name': 'Маша'},
-        {'first_name': 'Оля'},
-    ],[  # это – третий класс
-        {'first_name': 'Женя'},
-        {'first_name': 'Петя'},
-        {'first_name': 'Женя'},
-        {'first_name': 'Саша'},
-    ],
-]
-# ???
+def popular_name_students():
+    school_students = [
+        [  # это – первый класс
+            {'first_name': 'Вася'},
+            {'first_name': 'Вася'},
+        ],
+        [  # это – второй класс
+            {'first_name': 'Маша'},
+            {'first_name': 'Маша'},
+            {'first_name': 'Оля'},
+        ], [  # это – третий класс
+            {'first_name': 'Женя'},
+            {'first_name': 'Петя'},
+            {'first_name': 'Женя'},
+            {'first_name': 'Саша'},
+        ],
+    ]
+
+    count = 0
+    for school in school_students:
+        counts = {}
+        for student in school:
+            if student["first_name"] not in counts.keys():
+                counts[student["first_name"]] = 1
+            else:
+                counts[student["first_name"]] += 1
+        count += 1
+
+        max_count = max(counts, key=counts.get)
+        print(f"Самое частое имя среди учеников {count} класса: {max_count}, оно встречается {counts.get(max_count)}")
 
 
 # Задание 4
@@ -60,19 +92,30 @@ school_students = [
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
 
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2в', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
-]
-is_male = {
-    'Олег': True,
-    'Маша': False,
-    'Оля': False,
-    'Миша': True,
-    'Даша': False,
-}
-# ???
+def count_boy_and_girl():
+    school = [
+        {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+        {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+        {'class': '2в', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
+    ]
+    is_male = {
+        'Олег': True,
+        'Маша': False,
+        'Оля': False,
+        'Миша': True,
+        'Даша': False,
+    }
+
+    for cl in school:
+        boy = 0
+        girl = 0
+        for student in cl["students"]:
+            for name in student.values():
+                if is_male[name]:
+                    boy += 1
+                else:
+                    girl += 1
+        print(f"Класс {cl['class']}: девочки {girl}, мальчики {boy} ")
 
 
 # Задание 5
@@ -81,15 +124,39 @@ is_male = {
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
 
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
-is_male = {
-    'Маша': False,
-    'Оля': False,
-    'Олег': True,
-    'Миша': True,
-}
-# ???
+def class_comparison():
+    school = [
+        {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
+        {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
+    ]
+    is_male = {
+        'Маша': False,
+        'Оля': False,
+        'Олег': True,
+        'Миша': True,
+    }
 
+    for cl in school:
+        boy = 0
+        girl = 0
+        for student in cl["students"]:
+            for name in student.values():
+                if is_male[name]:
+                    boy += 1
+                else:
+                    girl += 1
+
+        if girl > boy:
+            print(f"Больше всего девочек в классе {cl['class']}")
+        elif boy == girl:
+            print(f"Девочек и мальчиков одинаковое количество в классе {cl['class']}")
+        else:
+            print(f"Больше всего мальчиков в классе {cl['class']}")
+
+
+if __name__ == "__main__":
+    count_student()
+    popular_name()
+    popular_name_students()
+    count_boy_and_girl()
+    class_comparison()
